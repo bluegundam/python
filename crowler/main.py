44bb4125b36a74b35e8e7f4b35b5420a863214c1
bluegundam <45892504+bluegundam@users.noreply.github.com>
2020-01-02 17:22:52 +0900

    최초 커밋

---
+++ crowler/main.py
@@ -0,0 +1,9 @@
+from indeed import get_jobs as get_indeed_jobs
+
+print("\n--start--\n")
+
+indeed_jobs = get_indeed_jobs()
+
+print(indeed_jobs)
+
+print("\n--end--\n")
---
+++ crowler/so.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+LIMIT = 50
+URL = f"https://stackoverflow.com/jobs?q=python&sort=i"
+#"https://stackoverflow.com/jobs?q=python&sort=i&pg=2"
+
+def get_last_page():
+
+
+#pagination 
+def get_jobs():
+  last_page = get_last_page()
+  return 
