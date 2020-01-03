d8cf67260cf49dfbf9aee7d5be53ebf6e252245f
bluegundam <45892504+bluegundam@users.noreply.github.com>
2020-01-03 18:11:37 +0900

    IP체크기능 완료, 메일발송기능 추가 중
    PUBLIC IP 체크기능 완료

--- ipcheck/main.py
+++ ipcheck/main.py
@@ -1,18 +1,55 @@
 import requests
 from bs4 import BeautifulSoup
 
+from common_mail import send_mail as sm
+
 
 
 #URL = "https://whatismyipaddress.com/"
 URL = "https://www.whatismyip.com/"
 agent = {"User-Agent":"Mozilla/5.0"}
 
-result = requests.get(URL, headers=agent)
-soup = BeautifulSoup(result.text, 'html.parser')
+# result = requests.get(URL, headers=agent)
+# soup = BeautifulSoup(result.text, 'html.parser')
 
-#pargination = soup.find("div", {"class": "pagination"})
 
-#target = soup.find("div", {"class" : "cf-error-footer cf-wrapper" })
+result_file = open('D:/600.workspace/00.python/python/ipcheck/aa1.html', mode='rt', encoding='utf-8')
+soup = BeautifulSoup(result_file, 'html.parser')
 
-print(soup)
-#print(target)
+card_body = soup.find("div", {"class" : "card-body"}).find_all("li", {"class" : "list-group-item"})
+
+public_ip = ""
+
+print("-----------------------")
+# print(card_body)
+for item in card_body:
+    print(item.string)
+
+    search = "My Public IPv4 is: "
+    if item.string is not None :
+        if search in item.string :
+            print("Hear !!!")
+            public_ip = item.string[len(search):]
+        else : 
+            print("None")
+    else :
+        print("String is null")
+
+    print("")
+print("-----------------------")
+
+print(f"Public IP : {public_ip}")
+
+result_file.close()
+
+# 확인된 public ip 메일 발송
+
+addr = 'backfire000@naver.com'
+subj_layout = 'Public IP share 2'
+cont_layout = f"Public IP : {public_ip}"
+
+try:
+    sm(addr, subj_layout, cont_layout)
+except Exception as ex:
+    print('에러발생', ex)
+
